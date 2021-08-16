ee6ec756b544065b1f346cb18a18d8191ec8bfb0
Anderson Lasak <anderson.lasak@petlove.com.br>
2021-08-16 17:52:38 -0300

    Adicionando jetpack navigation

--- app/src/main/java/br/com/petlove/androidteamprogramming/MainActivity.kt
+++ app/src/main/java/br/com/petlove/androidteamprogramming/MainActivity.kt
@@ -1,11 +1,15 @@
 package br.com.petlove.androidteamprogramming
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.petlove.androidteamprogramming.navigation.FragmentA
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .replace(android.R.id.content, FragmentA())
+            .commit()
     }
 }
