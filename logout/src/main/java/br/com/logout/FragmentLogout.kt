0b20d97974715fe631bc3ea99da04089df98c0b4
Renan Nunes <renan.nuness95@gmail.com>
2021-08-30 16:29:40 -0300

    Ativação do binding nos módulos e layouts das fragments

--- logout/src/main/java/br/com/logout/FragmentLogout.kt
+++ logout/src/main/java/br/com/logout/FragmentLogout.kt
@@ -1,6 +1,16 @@
 package br.com.logout
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import br.com.logout.databinding.FragmentLogoutBinding
 
 class FragmentLogout : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val binding = FragmentLogoutBinding.bind(view)
+        binding.button.setOnClickListener {
+
+        }
+    }
 }
--- storage/src/main/java/br/com/storage/FragmentStorage.kt
+++ storage/src/main/java/br/com/storage/FragmentStorage.kt
@@ -1,6 +1,13 @@
 package br.com.storage
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import br.com.storage.databinding.FragmentStorageBinding
 
 class FragmentStorage : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val binding = FragmentStorageBinding.bind(view)
+    }
 }
